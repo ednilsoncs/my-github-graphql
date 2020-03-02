15db71ea9cc361b3914e8e5584048d2424e49996
ednilsonCS <ednilsoncs@dcomp.ufs.br>
2020-03-02 15:47:19 -0300

    feat: Written as a query query to use in the application

--- src/screens/dashboard/index.js
+++ src/screens/dashboard/index.js
@@ -3,27 +3,75 @@ import gql from "graphql-tag";
 import { apollo } from "../../services";
 
 function Main(props) {
-  const [state, setState] = useState({});
-  const GET_EXEMPLE = gql`
-    query FindIssueID {
-      repository(owner: "octocat", name: "Hello-World") {
-        issue(number: 349) {
+  const [value, setValue] = useState("");
+  const [search, setSearch] = useState({});
+  const [repository, setRepository] = useState("");
+  const GET_REPOSITORY_USING_ID = gql`
+    query searchRepositoryUsingID($id: ID!) {
+      node(id: $id) {
+        ... on Repository {
           id
+          url
+          name
+          descriptionHTML
         }
       }
     }
   `;
-  const onFetchGithub = async () => {
-    const result = await apollo.query({
-      query: GET_EXEMPLE
-    });
-    return result;
+  const GET_SEARCH_REPOSITORY = gql`
+    query SearchRepository($queryString: String!) {
+      search(query: $queryString, type: REPOSITORY, first: 100) {
+        repositoryCount
+        edges {
+          node {
+            ... on Repository {
+              id
+              name
+            }
+          }
+        }
+      }
+    }
+  `;
+  const onFetchSearchUsignID = async id => {
+    try {
+      const { data } = await apollo.query({
+        query: GET_REPOSITORY_USING_ID,
+        variables: { id }
+      });
+      setRepository(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const onFetchSearchGithub = async queryString => {
+    try {
+      const { data } = await apollo.query({
+        query: GET_SEARCH_REPOSITORY,
+        variables: { queryString }
+      });
+      setSearch(data);
+    } catch (e) {
+      console.log(e);
+    }
   };
   return (
-    <dev>
-      {console.log(onFetchGithub())}
-      <span>Logado</span>
-    </dev>
+    <div>
+      <input
+        placeholder="Search"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          onFetchSearchUsignID("MDEwOlJlcG9zaXRvcnkyMDM0MDIz");
+          onFetchSearchGithub(value);
+        }}
+        type="button"
+      >
+        SEARCH
+      </button>
+    </div>
   );
 }
 
