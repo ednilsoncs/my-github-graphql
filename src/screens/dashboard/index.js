542dbf9d77dc65bf368813d876ea48cad678e131
ednilsonCS <ednilsoncs@dcomp.ufs.br>
2020-03-04 13:26:09 -0300

    feat: screen finish

--- src/screens/dashboard/index.js
+++ src/screens/dashboard/index.js
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import "./style.css";
+import React, { useState, useCallback } from "react";
 import gql from "graphql-tag";
+import { debounce } from "lodash";
 import { apollo } from "../../services";
 
 function Main(props) {
   const [value, setValue] = useState("");
-  const [search, setSearch] = useState({});
+  const [search, setSearch] = useState({ edges: [] });
   const [repository, setRepository] = useState("");
+
   const GET_REPOSITORY_USING_ID = gql`
     query searchRepositoryUsingID($id: ID!) {
       node(id: $id) {
@@ -39,7 +42,7 @@ function Main(props) {
         query: GET_REPOSITORY_USING_ID,
         variables: { id }
       });
-      setRepository(data);
+      setRepository(data.node);
     } catch (e) {
       console.log(e);
     }
@@ -50,27 +53,54 @@ function Main(props) {
         query: GET_SEARCH_REPOSITORY,
         variables: { queryString }
       });
-      setSearch(data);
+      setSearch(data.search);
     } catch (e) {
       console.log(e);
     }
   };
+  const handleDelayedInput = useCallback(
+    debounce(onFetchSearchGithub, 500),
+    []
+  );
   return (
     <div>
       <input
         placeholder="Search"
         value={value}
-        onChange={e => setValue(e.target.value)}
+        onChange={e => {
+          handleDelayedInput(e.target.value);
+          setValue(e.target.value);
+        }}
       />
       <button
         onClick={() => {
-          onFetchSearchUsignID("MDEwOlJlcG9zaXRvcnkyMDM0MDIz");
           onFetchSearchGithub(value);
         }}
         type="button"
       >
         SEARCH
       </button>
+      <div className="geral">
+        <div className="coluna_esquerda">
+          {search.edges.map(e => (
+            <button
+              key={e.node.id}
+              className="button_result"
+              type="button"
+              onClick={() => onFetchSearchUsignID(e.node.id)}
+            >
+              <span>{e.node.name}</span>
+            </button>
+          ))}
+        </div>
+        <div className="coluna_direita">
+          <span>O repostorio: {repository.name}</span>
+          <br />
+          <span>ID: {repository.id}</span>
+          <br />
+          <span>URL: {repository.url}</span>
+        </div>
+      </div>
     </div>
   );
 }
