ce96cb46731a5e33dcef7f8cd02cbae35a3aefbf
ednilsonCS <ednilsoncs@dcomp.ufs.br>
2020-03-04 13:32:08 -0300

    fix: remove screen login

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
-import Login from "./screens/login";
 import Dashboard from "./screens/dashboard";
 
 export default function AppRoutes() {
   return (
     <Router>
-      <Route path="/" exact component={Login} />
-      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/" component={Dashboard} />
     </Router>
   );
 }
